d744896e68ae18f755e55979840ed6d17b5b2736
Sultan Mustafin <sultan.fb29@gmail.com>
2022-11-22 16:52:07 +0300

    feat: create database connection and install typeorm package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfiguration } from './common/configurations/database.configuration';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: databaseConfiguration.host,
+      port: databaseConfiguration.port,
+      username: databaseConfiguration.username,
+      password: databaseConfiguration.password,
+      database: databaseConfiguration.database,
+      entities: [],
+      migrations: ['../database/migrations/*{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
 })
 export class AppModule {}
