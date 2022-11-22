f31981f9960a59335f78c868fbafbd51fdd43e9d
Sultan Mustafin <sultan.fb29@gmail.com>
2022-11-22 19:27:19 +0300

    feat: create user create repository methods

--- src/modules/users/domain/users.service.ts
+++ src/modules/users/domain/users.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { User } from "src/common/entities/user";
+import { CreateUserDto } from "../dtos/create-user.dto";
+import { UsersRepository } from "../repository/users.repository";
 
 @Injectable()
 export class UsersService {
-  
+  constructor(
+    private readonly usersRepository: UsersRepository
+  ) {}
+
+  async create(payload: CreateUserDto): Promise<User> {
+    
+    
+    return this.usersRepository.insertAndFetch(payload)
+  }
 }
--- src/modules/users/repository/users.repository.ts
+++ src/modules/users/repository/users.repository.ts
@@ -1,5 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { UserDao } from './dao/user.dao';
+import { CreateUserDto } from '../dtos/create-user.dto';
+import { User } from 'src/common/entities/user';
 
 @Injectable()
 export class UsersRepository {
+  constructor(
+    @InjectRepository(UserDao)
+    private readonly usersRepository: Repository<UserDao>,
+  ) {}
+
+  insertAndFetch(payload: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create(payload);
+
+    return this.usersRepository.save(user);
+  }
+
+  findOneByEmail(email: string): Promise<User> {
+    return this.usersRepository.findOneBy({
+      email,
+    });
+  }
 }
